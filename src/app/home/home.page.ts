138a41c3f67a1e2f849054108fb14fc83c0cfa07
Lucas Santiago <lsfdm09082002@gmail.com>
2020-10-30 09:28:20 -0300

    pokeapi

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  offset = 0;
+  pokemon = [];
 
-  constructor() {}
+  constructor(private userService: UserService) { }
 
+  ngOnInit() {
+    this.loadPokemon();
+  }
+
+  loadPokemon() {
+    this.userService.getPokemon(this.offset).subscribe(res => {
+      console.log('result: ', res);
+      this.pokemon = res;
+    })
+  }
 }
+
